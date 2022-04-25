e6ed0a69fa3d382976512121b372472badc79d92
chanaka subasingha <chanaka.s@icloud.com>
2022-04-25 12:59:49 +0200

    modified index file

--- src/index.js
+++ src/index.js
@@ -73,10 +73,10 @@ io.on('connection', (socket) => {
     });
 });
 
-app.use(express.static("../client/build"));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 server.listen(PORT, () => {
